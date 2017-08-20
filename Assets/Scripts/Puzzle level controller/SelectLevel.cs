4f2e1106e5b8d400fb910439de7bd1ac8dcf267b
PaxSentry <paxsentry@gmail.com>
2017-08-20 11:30:01 +0100

    Puzzle selection and back working

---
+++ Assets/Scripts/Puzzle level controller/SelectLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectLevel : MonoBehaviour
+{
+   [SerializeField]
+   private GameObject selectPuzzlePanel, puzzleLevelPanel;
+
+   [SerializeField]
+   private Animator selectPuzzleAnim, puzzleLevelAnim;
+
+   private string selectedPuzzle;
+
+   public void BackToPuzzleSelectMenu()
+   {
+      StartCoroutine(ShowPuzzleSelectMenu());
+   }
+
+   IEnumerator ShowPuzzleSelectMenu()
+   {
+      selectPuzzlePanel.SetActive(true);
+      selectPuzzleAnim.Play("SelectSlideIn");
+      puzzleLevelAnim.Play("LevelSelectorSlideOut");
+      yield return new WaitForSeconds(1f);
+      puzzleLevelPanel.SetActive(false);
+   }
+}
--- Assets/Scripts/Puzzle menu controller/SelectPuzzle.cs
+++ Assets/Scripts/Puzzle menu controller/SelectPuzzle.cs
@@ -1,19 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class SelectPuzzle : MonoBehaviour
 {
    [SerializeField]
-   private GameObject selectPuzzlePanel;
+   private GameObject selectPuzzlePanel, puzzleLevelSelector;
 
    [SerializeField]
-   private Animator selectPuzzleAnim;
+   private Animator selectPuzzleAnim, puzzleLevelAnim;
 
    private string selectPuzzle;
 
    public void SelectedPuzzle()
    {
-      selectPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+     // selectPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
 
-      Debug.Log(selectPuzzle);
+      StartCoroutine(ShowPuzzleLevelSeletor());
+   }
+
+   IEnumerator ShowPuzzleLevelSeletor()
+   {
+      puzzleLevelSelector.SetActive(true);
+      selectPuzzleAnim.Play("SelectSlideOut");
+      puzzleLevelAnim.Play("LevelSelectorSlideIn");
+      yield return new WaitForSeconds(1f);
+      selectPuzzlePanel.SetActive(false);
    }
 }
