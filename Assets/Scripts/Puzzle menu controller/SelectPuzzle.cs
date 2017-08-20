5c5c309ad77f95491a98171b3076d0be28ebe204
PaxSentry <paxsentry@gmail.com>
2017-08-20 10:41:55 +0100

    Menu scripts started

---
+++ Assets/Scripts/Puzzle menu controller/SelectPuzzle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SelectPuzzle : MonoBehaviour
+{
+   [SerializeField]
+   private GameObject selectPuzzlePanel;
+
+   [SerializeField]
+   private Animator selectPuzzleAnim;
+
+   private string selectPuzzle;
+
+   public void SelectedPuzzle()
+   {
+      selectPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+      Debug.Log(selectPuzzle);
+   }
+}
---
+++ Assets/Scripts/Puzzle menu controller/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+
+   [SerializeField]
+   private GameObject settingsPanel;
+
+   [SerializeField]
+   private Animator settingsAnim;
+
+   public void OpenSettingsPanel()
+   {
+      settingsPanel.SetActive(true);
+      settingsAnim.Play("SettingsSlideIn");
+   }
+
+   public void CloseSettingsPanel()
+   {
+      StartCoroutine(CloseSettings());
+   }
+
+   IEnumerator CloseSettings()
+   {
+      settingsAnim.Play("SettingsSlideOut");
+      yield return new WaitForSeconds(1f);
+      settingsPanel.SetActive(false);
+   }
+}
